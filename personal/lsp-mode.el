11efd30266df321af8e2c1e7ec2208f1e9ab9660
Thorsten <thor@goopy.fritz.box>
2023-01-16 14:02:56 +0100

    lsp-mode: do not show docs on mouse hover

--- personal/lsp-mode.el
+++ personal/lsp-mode.el
@@ -2,6 +2,7 @@
 (prelude-require-packages '(clojure-mode lsp-mode cider lsp-treemacs flycheck company))
 
 (add-hook 'clojure-mode-hook 'lsp)
+;(add-hook 'clojure-mode-hook 'lsp-deferred)
 (add-hook 'clojurescript-mode-hook 'lsp)
 (add-hook 'clojurec-mode-hook 'lsp)
 
@@ -11,4 +12,7 @@
       company-minimum-prefix-length 1
       lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
       ;; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+      lsp-ui-doc-show-with-mouse nil ; uncomment to suppress mouse hover
+      ;; lsp-signature-auto-activate nil ;uncomment to suppress signature help.
+      ;; prevent watching huge .cache dir (greetings, bazel...)
       )
