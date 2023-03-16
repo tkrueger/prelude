97ec1bbed7ccd46eee8c6e5a1745d2af5f81b29a
thork <thorstenkg@gmail.com>
2023-03-16 20:10:54 +0100

    add magit and marginalia configs

---
+++ personal/magit.el
@@ -0,0 +1,10 @@
+;; load magit-forge
+
+;;(unless (package-installed-p 'forge)
+;;  (package-install 'forge))
+
+;;(with-eval-after-load 'magit
+;;  (require 'forge))
+
+;; only use the one file for authentication
+;;(setq auth-sources '("~/.authinfo"))
---
+++ personal/marginalia.el
@@ -0,0 +1,16 @@
+;; see https://github.com/minad/marginalia
+
+;; Enable rich annotations using the Marginalia package
+(prelude-require-package 'marginalia)
+(use-package marginalia
+  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
