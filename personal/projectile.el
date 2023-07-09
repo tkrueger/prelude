6dc61ffdb5958ee51b73871a626eba817aaad39d
thork <thorstenkg@gmail.com>
2023-07-09 16:38:47 +0200

    WIP: registering bb projects

--- personal/projectile.el
+++ personal/projectile.el
@@ -1,3 +1,7 @@
 ;; Set native indexing mode, to make projectile list git-ignored files.
 ;; see https://docs.projectile.mx/projectile/configuration.html
 (setq projectile-indexing-method 'alien)
+
+(projectile-register-project-type 'bb '("bb.edn")
+                                  :project-file "bb.edn"
+                                  :test "bb test")
