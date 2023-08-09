82778666c974a54d1568a7d212126bc577270733
thork <thorstenkg@gmail.com>
2023-08-09 08:04:38 +0200

    correct kaocha-run-tests command

--- personal/kaocha.el
+++ personal/kaocha.el
@@ -5,7 +5,7 @@
   (defvar kaocha
     (let ((map (make-sparse-keymap)))
       (define-key map "t" 'kaocha-runner-run-test-at-point)
-      (define-key map "r" 'kaocha-runner-run-test)
+      (define-key map "r" 'kaocha-runner-run-tests)
       (define-key map "a" 'kaocha-runner-run-all-tests)
       (define-key map "w" 'kaocha-runner-show-warnings)
       (define-key map "h" 'kaocha-runner-hide-windows)
